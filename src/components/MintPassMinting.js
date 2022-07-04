93576bba07792f1d29d1b265ab08746654ec4308
vantrong291 <vantrong291@gmail.com>
2022-07-04 15:25:53 +0700

    [UI] updated Loading Wrapper

--- src/components/MintPassMinting.js
+++ src/components/MintPassMinting.js
@@ -23,7 +23,7 @@ const MintPassMinting = (props) => {
     const [walletInfo, setWalletInfo] = useState({})
 
     const {user, onConnect} = useContext(AuthContext)
-    const {setLoading} = useContext(AppContext)
+    const {loading, setLoading} = useContext(AppContext)
 
     useEffect(() => {
         fetchData().then()
@@ -178,104 +178,105 @@ const MintPassMinting = (props) => {
                      width="70"
                      height="70" className="move-vertical-reversed"/>
             </div>
-            <div className="section-content">
-                <div className="container">
-
-                    <div className={'flex justify-center items-start'}>
-                        <div className={'flex mr-5'}>
-                            <h2 className={'font-bold text-3xl secondary-color'}>Mint Pass Minting</h2>
-                        </div>
-                        <div className={'flex'}>
-                            {isActive ? (
-                                <span
-                                    className="bg-green-400 text-green-800 text-sm font-bold mr-2 px-2.5 py-0.5 rounded dark:bg-green-500 dark:text-white">
+            {
+                !loading && <div className="section-content">
+                    <div className="container">
+                        <div className={'flex justify-center items-start'}>
+                            <div className={'flex mr-5'}>
+                                <h2 className={'font-bold text-3xl secondary-color'}>Mint Pass Minting</h2>
+                            </div>
+                            <div className={'flex'}>
+                                {isActive ? (
+                                    <span
+                                        className="bg-green-400 text-green-800 text-sm font-bold mr-2 px-2.5 py-0.5 rounded dark:bg-green-500 dark:text-white">
                                     Active
                                 </span>
-                            ) : (
-                                <span
-                                    className="bg-indigo-100 text-indigo-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-900">
+                                ) : (
+                                    <span
+                                        className="bg-indigo-100 text-indigo-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-900">
                                     Inactive
                                 </span>
-                            )}
-                        </div>
-                    </div>
-                    <div className="moonfit-card">
-                        <div className="moonfit-card-inner">
-                            <div className="card-title">
-                                Minting information
+                                )}
                             </div>
-                            <div className="card-body">
-                                <div className={'mt-8'}>
-                                    <div className={'flex justify-between'}>
-                                        <div className={'flex'}>Mint Pass contract</div>
-                                        <div className={'flex flex-col'}>
-                                            <div className={'flex text-green-500'}>
-                                                {contract}
-                                            </div>
-                                            <div className={'flex justify-end'}>
-                                                {renderAddressLink(contract)}
+                        </div>
+                        <div className="moonfit-card">
+                            <div className="moonfit-card-inner">
+                                <div className="card-title">
+                                    Minting information
+                                </div>
+                                <div className="card-body">
+                                    <div className={'mt-8'}>
+                                        <div className={'flex justify-between'}>
+                                            <div className={'flex'}>Mint Pass contract</div>
+                                            <div className={'flex flex-col'}>
+                                                <div className={'flex text-green-500'}>
+                                                    {contract}
+                                                </div>
+                                                <div className={'flex justify-end'}>
+                                                    {renderAddressLink(contract)}
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    {/*<div className={'flex justify-between'}>*/}
-                                    {/*    <div className={'flex'}>Purchase price</div>*/}
-                                    {/*    <div className={'flex text-green-500'}>{1}</div>*/}
-                                    {/*</div>*/}
-                                    {/*<div className={'flex justify-between'}>*/}
-                                    {/*    <div className={'flex'}>Sale rate</div>*/}
-                                    {/*    <div className={'flex text-green-500'}>{1} (1 GLMR = {1} MFG)*/}
-                                    {/*    </div>*/}
-                                    {/*</div>*/}
-                                    {/*<div className={'flex justify-between'}>*/}
-                                    {/*    <div className={'flex'}>Remaining slot</div>*/}
-                                    {/*    <div className={'flex text-green-500'}>{1}</div>*/}
-                                    {/*</div>*/}
-                                    <hr className={'my-4'}/>
-                                    <div className={'flex justify-between'}>
-                                        <div className={'flex'}>Your Mint Pass Balance</div>
-                                        <div className={'flex text-green-500'}>{balance || "0"} NFT</div>
-                                    </div>
-                                    <div className={'flex justify-between'}>
-                                        <div className={'flex'}>Token ID</div>
-                                        <div
-                                            className={'flex text-green-500'}>{renderNFTLink(contract, tokenId) || "Empty"}</div>
-                                    </div>
-                                    <div className={'flex flex-row justify-center mt-4'}>
-                                        <button type="button"
-                                                onClick={handleMintMintPass}
-                                                disabled={mintLoading}
-                                                className="mt-3 button button-secondary flex items-center">
-                                            {
-                                                mintLoading ? (
-                                                    <svg role="status"
-                                                         className="inline w-4 h-4 mr-3 text-white animate-spin"
-                                                         viewBox="0 0 100 101" fill="none"
-                                                         xmlns="http://www.w3.org/2000/svg">
-                                                        <path
-                                                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                                            fill="#E5E7EB"/>
-                                                        <path
-                                                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                                            fill="currentColor"/>
-                                                    </svg>
-                                                ) : (
-                                                    <svg className="inline w-6 h-6 mr-2" fill="none"
-                                                         stroke="currentColor"
-                                                         viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                        <path strokeLinecap="round" strokeLinejoin="round"
-                                                              strokeWidth={2}
-                                                              d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"/>
-                                                    </svg>
-                                                )
-                                            } Mint A Pass
-                                        </button>
+                                        {/*<div className={'flex justify-between'}>*/}
+                                        {/*    <div className={'flex'}>Purchase price</div>*/}
+                                        {/*    <div className={'flex text-green-500'}>{1}</div>*/}
+                                        {/*</div>*/}
+                                        {/*<div className={'flex justify-between'}>*/}
+                                        {/*    <div className={'flex'}>Sale rate</div>*/}
+                                        {/*    <div className={'flex text-green-500'}>{1} (1 GLMR = {1} MFG)*/}
+                                        {/*    </div>*/}
+                                        {/*</div>*/}
+                                        {/*<div className={'flex justify-between'}>*/}
+                                        {/*    <div className={'flex'}>Remaining slot</div>*/}
+                                        {/*    <div className={'flex text-green-500'}>{1}</div>*/}
+                                        {/*</div>*/}
+                                        <hr className={'my-4'}/>
+                                        <div className={'flex justify-between'}>
+                                            <div className={'flex'}>Your Mint Pass Balance</div>
+                                            <div className={'flex text-green-500'}>{balance || "0"} NFT</div>
+                                        </div>
+                                        <div className={'flex justify-between'}>
+                                            <div className={'flex'}>Token ID</div>
+                                            <div
+                                                className={'flex text-green-500'}>{renderNFTLink(contract, tokenId) || "Empty"}</div>
+                                        </div>
+                                        <div className={'flex flex-row justify-center mt-4'}>
+                                            <button type="button"
+                                                    onClick={handleMintMintPass}
+                                                    disabled={mintLoading}
+                                                    className="mt-3 button button-secondary flex items-center">
+                                                {
+                                                    mintLoading ? (
+                                                        <svg role="status"
+                                                             className="inline w-4 h-4 mr-3 text-white animate-spin"
+                                                             viewBox="0 0 100 101" fill="none"
+                                                             xmlns="http://www.w3.org/2000/svg">
+                                                            <path
+                                                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                                fill="#E5E7EB"/>
+                                                            <path
+                                                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                                fill="currentColor"/>
+                                                        </svg>
+                                                    ) : (
+                                                        <svg className="inline w-6 h-6 mr-2" fill="none"
+                                                             stroke="currentColor"
+                                                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                  strokeWidth={2}
+                                                                  d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"/>
+                                                        </svg>
+                                                    )
+                                                } Mint A Pass
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            }
         </WalletAuthRequired>
     )
 }
