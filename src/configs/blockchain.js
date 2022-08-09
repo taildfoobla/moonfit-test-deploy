e5ce958bf4874c96af711787ec30221f8d115a4d
vantrong291 <vantrong291@gmail.com>
2022-08-09 11:52:59 +0700

    [UI] updated using upgradeable contracts

--- src/configs/blockchain.js
+++ src/configs/blockchain.js
@@ -7,8 +7,8 @@ const getConfigs = () => {
             return {
                 MOONBEAM_SCAN_URL: 'https://moonbase.moonscan.io',
                 MINT_PASS_SC: '0x7E7d9fee5c5994aA7FC1dAeb231Af015e2FdAD3E',
-                R1_NFT_SALE_SC: '0x9676D390E28D6d00aA8Cc666E3ad52E974B2e4E3',
-                MOONBEAST_SC: '0x21d95dAC89c5665F31777E3f9aF2A997dAc4eCF7',
+                R1_NFT_SALE_SC: '0x1D4632DE3E1AD3F495903b65b9F6C7e5b5035755',
+                MOONBEAST_SC: '0xEb35396158bC0918ED3d06Bcba3FebE162839c30',
             }
         case 'production':
             return {
