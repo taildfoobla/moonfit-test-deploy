fa144713086fa8f43027f4b87312502ed4ace0d4
vantrong291 <vantrong291@gmail.com>
2022-08-18 17:27:26 +0700

    [Prod] enabled NFT Sale page

--- src/constants/blockchain.js
+++ src/constants/blockchain.js
@@ -199,8 +199,8 @@ export const NFT_SALE_INFO = {
         // price: 0.01, // TODO prod
         nftPerPass: 1,
         // time: new Date().getTime() + 10000,
-        time: 1660817942296,
-        // time: 1661176800000
+        // time: 1660817942296,
+        time: 1661176800000
     },
     R2: {
         title: 'Whitelist Sale #2',
