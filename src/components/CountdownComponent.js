4a69993b789ae3a0118c4502db5d2e03ee0c4a97
vantrong291 <vantrong291@gmail.com>
2022-08-18 17:33:00 +0700

    [Prod] enabled NFT Sale page

--- src/components/CountdownComponent.js
+++ src/components/CountdownComponent.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Countdown from "react-countdown"
+import {formatZeroNumber} from "../utils/number"
 
 export const CountdownComponent = ({date, completedMessage = 'You are good to go!', completedCallback = null}) => {
+
     const renderer = ({days, hours, minutes, seconds, completed}) => {
         if (completed) {
             if (completedCallback) {
@@ -13,10 +15,10 @@ export const CountdownComponent = ({date, completedMessage = 'You are good to go
         } else {
             return (
                 <div className={'flex countdown-container text-lg'}>
-                    <div className={'countdown countdown-date race-sport-font secondary-color'}>{days}</div>
-                    <div className={'countdown countdown-hour race-sport-font secondary-color'}>{hours}</div>
-                    <div className={'countdown countdown-minute race-sport-font secondary-color'}>{minutes}</div>
-                    <div className={'countdown countdown-second race-sport-font secondary-color'}>{seconds}</div>
+                    <div className={'countdown countdown-date race-sport-font secondary-color'}>{formatZeroNumber(days)}</div>
+                    <div className={'countdown countdown-hour race-sport-font secondary-color'}>{formatZeroNumber(hours)}</div>
+                    <div className={'countdown countdown-minute race-sport-font secondary-color'}>{formatZeroNumber(minutes)}</div>
+                    <div className={'countdown countdown-second race-sport-font secondary-color'}>{formatZeroNumber(seconds)}</div>
                 </div>
             )
         }
@@ -24,8 +26,8 @@ export const CountdownComponent = ({date, completedMessage = 'You are good to go
     return (
         <Countdown date={date}
                    renderer={renderer}
-                   zeroPadTime={2}
-                   zeroPadDays={2}
+                   // zeroPadTime={2}
+                   // zeroPadDays={2}
         />
     )
 }
