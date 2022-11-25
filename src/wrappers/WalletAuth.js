3eea6e7a627b636658925b72c0413f57fd557308
Hoàng Nhâm <hoangnham01@gmail.com>
2022-11-25 16:59:55 +0700

    add debug metamask

--- src/wrappers/WalletAuth.js
+++ src/wrappers/WalletAuth.js
@@ -49,6 +49,28 @@ const WalletAuthWrapper = ({ children }) => {
         }
 
         checkConnectedWallet()
+
+        /****
+         * TODO: For debug
+         */
+        function getParameterByName(name, url = window.location.href) {
+            name = name.replace(/[\[\]]/g, '\\$&');
+            const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, ' '));
+        }
+
+        const debug = getParameterByName('_debug')
+
+        if (debug) {
+            if (debug === '1') {
+                localStorage.setItem('_debug', '1')
+            } else {
+                localStorage.removeItem('_debug')
+            }
+        }
     }, [])
 
     const detectProvider = useCallback((providerNameParam = null) => {
@@ -186,12 +208,32 @@ const WalletAuthWrapper = ({ children }) => {
     const onWalletSelect = async (wallet) => {
         const providerName = wallet.extensionName
         const isInstalled = window[providerName] && window[providerName][wallet.isSetGlobalString]
+
+        if (localStorage.getItem('_debug')) {
+            alert(JSON.stringify({
+                providerName: providerName,
+                isSetGlobalString: wallet.isSetGlobalString,
+                w: window[providerName],
+                isInstalled,
+            }))
+        }
+
         if (isMobileOrTablet() && !isMetaMaskBrowser) {
-            if (providerName === "SubWallet") deepLink = `subwallet://browser?url=${window.location.host}`
-            if (pathname && pathname.length !== 0) deepLink = `${deepLink}${pathname}`
+            if (providerName === "SubWallet") {
+                deepLink = `subwallet://browser?url=${window.location.host}`
+            }
+
+            if (pathname && pathname.length !== 0)  {
+                deepLink = `${deepLink}${pathname}`
+            }
+
             return window.location.href = deepLink
         }
-        if (!isInstalled) return window.open(wallet.installUrl)
+
+        if (!isInstalled) {
+            return window.open(wallet.installUrl)
+        }
+
         setWalletExtKey(providerName)
         await onConnect(providerName)
         hideConnectModal()
