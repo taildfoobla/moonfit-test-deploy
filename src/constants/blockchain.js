e456f9f6c06a295cc150ed09eb87ebf0a0c1ee9d
nhamhv <nhamhv@foobla.com>
2023-10-05 17:31:13 +0700

    update

--- src/constants/blockchain.js
+++ src/constants/blockchain.js
@@ -269,7 +269,7 @@ const NETWORKS = [
     {
         chainId: 3441005,
         chainHex: '0x54792197',
-        digit: 4,
+        digit: 5,
         currencyDecimal: 18,
         networkName: 'Manta Pacific Testnet',
         symbol: 'MANTA_ETH',
@@ -287,7 +287,7 @@ const NETWORKS = [
     {
         chainId: 169,
         chainHex: '0x361',
-        digit: 6,
+        digit: 5,
         currencyDecimal: 18,
         networkName: 'Manta Pacific',
         symbol: 'MANTA_ETH',
