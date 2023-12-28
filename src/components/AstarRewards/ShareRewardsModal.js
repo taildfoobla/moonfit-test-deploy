984e6ab3774424b540afea5ad55c39619dde2aa3
taild <taild@foobla.com>
2023-12-28 09:12:51 +0700

    update 28/12

--- src/components/AstarRewards/ShareRewardsModal.js
+++ src/components/AstarRewards/ShareRewardsModal.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Modal } from "antd";
 import RewardsImg from "../../assets/images/astar-rewards/share-rewards-img.png";
 import CloseBorder from "../../assets/images/astar-rewards/close-border.png";
-import CloseNoBorder from "../../assets/images/astar-rewards/close-no-border.png"
+import CloseNoBorder from "../../assets/images/astar-rewards/close-no-border.png";
+import Rewards from "../../assets/images/astar-rewards/share-rewards.png";
 
 export default function ShareRewardsModal({ isOpen, onClose }) {
   return (
@@ -13,13 +14,16 @@ export default function ShareRewardsModal({ isOpen, onClose }) {
       centered={true}
       footer={false}
     >
-      <div className="close-btn">
+      <div className="close-btn" onClick={onClose}>
         <img src={CloseBorder} alt="" />
-        <img src={CloseNoBorder} alt=""/>
+        <img src={CloseNoBorder} alt="" />
       </div>
       <h3 className="share-rewards-modal-header">Congratulations!</h3>
       <p>You have successfully shared the reward pool of</p>
-      <img src={RewardsImg} alt="" />
+      <div className="share-rewards-img">
+        <img src={RewardsImg} alt="" />
+        <img src={Rewards} alt="" />
+      </div>
       <p>Stake more to enhance your benefits with MoonFit:</p>
       <ul className="benefit-list">
         <li className="benefit-item">
@@ -45,8 +49,12 @@ export default function ShareRewardsModal({ isOpen, onClose }) {
         </li>
       </ul>
       <div className="button-wrapper">
-        <button className="share">Share & Earn More ASTR</button>
-        <button className="stake">Stake More</button>
+        <button className="share" onClick={()=>{
+          window.open("https://twitter.com/intent/tweet?url=https%3A%2F%2Fmoonfit-token-sale.web.app%2F&text=MoonFit&hashtags=MoonFit")
+        }}>Share & Earn More ASTR</button>
+        <button className="stake" onClick={()=>{
+          window.open("https://portal.astar.network/astar/dapp-staking/dapp?dapp=0xe785a37c9d5f3377cbb5b8bf7e9db03ddd440449")
+        }}>Stake More</button>
       </div>
     </Modal>
   );
