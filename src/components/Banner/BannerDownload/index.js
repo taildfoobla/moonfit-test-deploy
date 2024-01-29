06780942890fd48c8c3ead6933cda64d943372a5
taild <taild@foobla.com>
2024-01-29 09:44:38 +0700

    update logic

--- src/components/Banner/BannerDownload/index.js
+++ src/components/Banner/BannerDownload/index.js
@@ -4,14 +4,18 @@ import BannerBackground from "../../../assets/images/banner/banner-bg.png"
 import GooglePlay from "../../../assets/images/banner/google-play.png"
 import AppStore from "../../../assets/images/banner/app-store.png"
 import {Link} from "react-router-dom"
-import {Carousel} from "antd"
+import {Carousel, message as AntdMessage} from "antd"
 import "./style.less"
 import HightlightEvent from "../../HightlightEvent"
 import NextSlider from "../../../assets/images/banner/next-slider.png"
 import PrevSlider from "../../../assets/images/banner/prev-slider.png"
+import {useAuth} from "../../../core/contexts/auth"
 
 export default function BannerDownload(props) {
     const {highlightEvent} = props
+
+    const {auth,isLoginSocial}=useAuth()
+
     const handleOpenLink = (url) => {
         window.open(url)
     }
@@ -32,6 +36,17 @@ export default function BannerDownload(props) {
     const handlePrevSlider = () => {
         console.log("prev")
         sliderRef.current.prev()
+    }
+
+    const handlePreventPassthrough=(e)=>{
+        if(!isLoginSocial && !auth?.isConnected){
+            e.preventDefault()
+            return AntdMessage.error({
+                key:"err",
+                content: "Please connect wallet and login social",
+                className: "message-error",
+                duration: 5,})
+        }
     }
 
     return (
@@ -69,7 +84,9 @@ export default function BannerDownload(props) {
                     <Carousel dots={false} ref={sliderRef} autoplay={false}>
                         {highlightEvent.map((event, index) => (
                             <div key={index} className="hightlight-event-container">
-                                <Link className="hightlight-event-bg" to={`/special-event/${event.event_type.title}`}>
+                                <Link className="hightlight-event-bg" to={`/special-event/${event.event_type.title}`} onClick={(e)=>{
+                                    handlePreventPassthrough(e)
+                                }}>
                                     <img src={event?.banner_event?.url} alt="" />
                                     <div className="hightlight-event-type">HOT</div>
                                 </Link>{" "}
