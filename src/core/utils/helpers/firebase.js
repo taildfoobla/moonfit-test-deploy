fbe7be1afd332ab0fce3f20f01633c4948436b9f
nhamhv <nhamhv@foobla.com>
2024-01-30 12:01:56 +0700

    update

--- src/core/utils/helpers/firebase.js
+++ src/core/utils/helpers/firebase.js
@@ -12,10 +12,13 @@ import {connectWalletToAccountAPI} from "../../services/connect-account"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const authDomain = 'app.moonfit.xyz'
+const authDomain = 'sw-move2earn-app-f8519.firebaseapp.com'
 export const firebaseConfig = {
     apiKey: "AIzaSyBaxQq4UR37DOnc5kF1UJI0jn5OLMK-fG8",
     // authDomain: "sw-move2earn-app-f8519.firebaseapp.com",
-    authDomain: "app.moonfit.xyz",
+    // authDomain: "app.moonfit.xyz",
+    authDomain,
 
     databaseURL: "https://sw-move2earn-app-f8519-default-rtdb.firebaseio.com",
     projectId: "sw-move2earn-app-f8519",
@@ -24,16 +27,6 @@ export const firebaseConfig = {
     appId: "1:619500821290:web:62bf1adf647deb739d5c53",
     measurementId: "G-6C18S0ENXE",
 }
-
-// export const firebaseConfig = {
-//   apiKey: "AIzaSyDeLto8NTAfLpNRNhTgokvHu_7Ahg0rEZA",
-//   authDomain: "login-firebase-d1f8d.firebaseapp.com",
-//   projectId: "login-firebase-d1f8d",
-//   storageBucket: "login-firebase-d1f8d.appspot.com",
-//   messagingSenderId: "31744565688",
-//   appId: "1:31744565688:web:f475ff7c30161a1c986f4d",
-//   measurementId: "G-9Q29CZ6HJB"
-// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
